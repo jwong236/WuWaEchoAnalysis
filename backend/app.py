d12bab49b07169522200a24667539ed496bfa24d
Jacob Wong <jacobsunsetbluff@gmail.com>
2024-06-21 17:39:58 -0700

    Deploying app to Heroku to test

--- backend/app.py
+++ backend/app.py
@@ -2,17 +2,17 @@ from flask import Flask, request, jsonify
 import psycopg2
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="dist")
 
-DB_NAME = os.getenv("DB_NAME", "yourdbname")
-DB_USER = os.getenv("DB_USER", "youruser")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "yourpassword")
-DB_HOST = os.getenv("DB_HOST", "localhost")
+# DB_NAME = os.getenv("DB_NAME", "yourdbname")
+# DB_USER = os.getenv("DB_USER", "youruser")
+# DB_PASSWORD = os.getenv("DB_PASSWORD", "yourpassword")
+# DB_HOST = os.getenv("DB_HOST", "localhost")
 
-conn = psycopg2.connect(
-    dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST
-)
-cursor = conn.cursor()
+# conn = psycopg2.connect(
+#     dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST
+# )
+# cursor = conn.cursor()
 
 
 @app.route("/")
