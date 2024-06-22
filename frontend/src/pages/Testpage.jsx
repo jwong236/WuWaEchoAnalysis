44648486c6d8ad5bb2b43e21011e1746b35dd4cb
Jacob Wong <jacobsunsetbluff@gmail.com>
2024-06-21 20:43:02 -0700

    Add debugging to figure out deployment issue

--- frontend/src/pages/Testpage.jsx
+++ frontend/src/pages/Testpage.jsx
@@ -1,18 +1,22 @@
 // src/pages/Testpage.jsx
 import React, { useEffect, useState } from "react";
 import { Box, Typography } from "@mui/material";
+import { useLocation } from "react-router-dom";
 
 function Testpage() {
   const [data, setData] = useState(null);
+  const location = useLocation();
 
   useEffect(() => {
+    console.log("Current path:", location.pathname);
+
     const apiUrl = import.meta.env.VITE_API_URL + "/data";
     console.log("API URL:", apiUrl);
     fetch(apiUrl)
       .then((response) => response.json())
       .then((data) => setData(data))
       .catch((error) => console.error("Error fetching data:", error));
-  }, []);
+  }, [location]);
 
   return (
     <Box
